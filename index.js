477de112da1a76cea68ce0ee75a730269563f429
Ekaterina <87katrod@gmail.com>
2018-12-11 15:48:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,42 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100) + 1;
+  var obj = {itemName:item, itemPrice:price};
+  
+ 
+  cart.push(obj);
+  return item + " has been added to your cart.";
+
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length ===0) {
+    return 'Your shopping cart is empty.'
+  }
+  let result = 'In your cart, you have ';
+  
+  for (var i=0; i<cart.length; i++) {
+    let element = cart[i];
+    if (cart.length ===1) {
+      result +=`${element.itemName} at $${element.itemPrice}.`
+    }
+  else if (i=== cart.length-1) {
+      result +=`and ${element.itemName} at $${element.itemPrice}.`
+    }
+    else {
+       result +=`${element.itemName} at $${element.itemPrice}, `
+    }
+    
+  }
+
+  return result;
 }
 
 function total() {
-  // write your code here
+  let total = 0;
+  total +=
+  return total;
 }
 
 function removeFromCart(item) {
